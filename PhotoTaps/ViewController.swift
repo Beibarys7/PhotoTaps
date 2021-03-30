d6d65f890f2464385860132b37eea0f7ba14e723
Beibarys <beibarys1927@gmail.com>
2021-03-30 21:24:47 +0600

    Initial Commit

---
+++ PhotoTaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTaps
+//
+//  Created by Бейбарыс Шагай on 3/30/21.
+//  Copyright © 2021 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
